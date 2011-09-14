dc97f2c8f81c908535df2918c5a9ce8a0ecfd548
ckian <ckian@mail.csuchico.edu>
2011-09-13 22:48:19 -0700

    done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 SampleApp::Application.routes.draw do
+  get "pages/home"
+  get "pages/contact"
+  get "pages/about"o
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
