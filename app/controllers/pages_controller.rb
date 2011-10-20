a3dffa27ed8294c818094dc36b8bed6d98754759
ckian <ckian@mail.csuchico.edu>
2011-10-20 14:21:08 -0700

    lesson 7 complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,49 @@
-class PagesController < ApplicationController
+require 'spec_helper'
 
-  def home
+describe PagesController do
+  render_views
+
+  describe "GET 'home'" do
+    it "should be successful" do
+      get 'home'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
-  def contact
+  describe "GET 'contact'" do
+    it "should be successful" do
+      get 'contact'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | Contact")
+    end
   end
 
-  def about
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                        :content =>
+                          "Ruby on Rails Tutorial Sample App | About")
+    end
+  end
+  def help
+    @title = "Help"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,18 +1,11 @@
-require 'spec_helper'
+equire 'spec_helper'
 
 describe PagesController do
-  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'home'
-      response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
@@ -20,27 +13,6 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'contact'
-      response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
-    end
-  end
-
-  describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
-
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
     end
   end
 end
